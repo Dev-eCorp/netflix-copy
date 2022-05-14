78a048c7070c6cc3fc1e7ee35ab58f5f13c79761
Eduard Cruz <salikintres2001@gmail.com>
2022-05-13 22:36:48 -0500

    [create] se crea parte del banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import { Movie } from '../typings'
 import requests from '../utils/requests'
 
-const Home = ({  }) => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
+
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -12,7 +34,7 @@ const Home = ({  }) => {
       </Head>
       <Header/>
       <main>
-        <Banner/>
+        <Banner netflixOriginals={netflixOriginals}/>
       </main>
     </div>
   )
